4908f88c8e23e2d5b5e6bb82a6f289d1003ecaa4
Viktor Kovacs <crusader8@gmail.com>
2022-12-08 14:21:19 +0100

    refactor

--- src/main/groovy/day8/day8.groovy
+++ src/main/groovy/day8/day8.groovy
@@ -2,91 +2,97 @@ package day8
 
 def lines = new File("../../resources/day8/input").readLines()
 
-final int forestSize = lines[0].size()
-
-List<List<Boolean>> forestVisibility = []
-forestSize.times {
-    forestVisibility << (0..forestSize - 1).collect { false }
-}
-
-
 def forest = []
 lines.forEach {
     forest << it.split("")
 }
 
 
+int part1(List<List<Integer>> forest) {
+    int forestSize = forest[0].size()
+
+    List<List<Boolean>> forestVisibility = []
+    forestSize.times {
+        forestVisibility << (0..forestSize - 1).collect { false }
+    }
+
 //left to right
-for (int i = 0; i < forestSize; i++) {
-    int max = 0
-    for (int j = 0; j < forestSize; j++) {
-        if (j == 0) {
-            max = forest[i][j]
-        }
-        if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
-            forestVisibility[i][j] = true
-        } else {
-            if (forest[i][j] > max) {
-                forestVisibility[i][j] = true
+    for (int i = 0; i < forestSize; i++) {
+        int max = 0
+        for (int j = 0; j < forestSize; j++) {
+            if (j == 0) {
                 max = forest[i][j]
+            }
+            if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
+                forestVisibility[i][j] = true
+            } else {
+                if (forest[i][j] > max) {
+                    forestVisibility[i][j] = true
+                    max = forest[i][j]
+                }
             }
         }
     }
-}
 
 // right to left
-for (int i = 0; i < forestSize; i++) {
-    int max = 0
-    for (int j = forestSize - 1; j >= 0; j--) {
-        if (j == forestSize - 1) {
-            max = forest[i][j]
-        }
-        if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
-            forestVisibility[i][j] = true
-        } else {
-            if (forest[i][j] > max) {
-                forestVisibility[i][j] = true
+    for (int i = 0; i < forestSize; i++) {
+        int max = 0
+        for (int j = forestSize - 1; j >= 0; j--) {
+            if (j == forestSize - 1) {
                 max = forest[i][j]
+            }
+            if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
+                forestVisibility[i][j] = true
+            } else {
+                if (forest[i][j] > max) {
+                    forestVisibility[i][j] = true
+                    max = forest[i][j]
+                }
             }
         }
     }
-}
 
 // up to down
-for (int j = 0; j < forestSize; j++) {
-    int max = 0
-    for (int i = 0; i < forestSize; i++) {
-        if (i == 0) {
-            max = forest[i][j]
-        }
-        if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
-            forestVisibility[i][j] = true
-        } else {
-            if (forest[i][j] > max) {
-                forestVisibility[i][j] = true
+    for (int j = 0; j < forestSize; j++) {
+        int max = 0
+        for (int i = 0; i < forestSize; i++) {
+            if (i == 0) {
                 max = forest[i][j]
+            }
+            if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
+                forestVisibility[i][j] = true
+            } else {
+                if (forest[i][j] > max) {
+                    forestVisibility[i][j] = true
+                    max = forest[i][j]
+                }
             }
         }
     }
-}
 //down to up
-for (int j = 0; j < forestSize; j++) {
-    int max = 0
-    for (int i = forestSize - 1; i >= 0; i--) {
-        if (i == forestSize - 1) {
-            max = forest[i][j]
-        }
-        if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
-            forestVisibility[i][j] = true
-        } else {
-            if (forest[i][j] > max) {
-                forestVisibility[i][j] = true
+    for (int j = 0; j < forestSize; j++) {
+        int max = 0
+        for (int i = forestSize - 1; i >= 0; i--) {
+            if (i == forestSize - 1) {
                 max = forest[i][j]
+            }
+            if (i == 0 || i == forestSize - 1 || j == 0 || j == forestSize - 1) {
+                forestVisibility[i][j] = true
+            } else {
+                if (forest[i][j] > max) {
+                    forestVisibility[i][j] = true
+                    max = forest[i][j]
+                }
             }
         }
     }
+
+    forestVisibility.flatten().count { it == true }
 }
 
-println forestVisibility
 
-println forestVisibility.flatten().count { it == true }
+def solution1 = part1(forest)
+println solution1
+assert solution1 == 1796
+
+
