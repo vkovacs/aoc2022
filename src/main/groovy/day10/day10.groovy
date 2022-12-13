1161b5e08bf68d0faa9980bf1f36a0075de4928b
Viktor Kovacs <crusader8@gmail.com>
2022-12-13 08:24:04 +0100

    day10 part2

--- src/main/groovy/day10/day10.groovy
+++ src/main/groovy/day10/day10.groovy
@@ -9,11 +9,14 @@ def solution(def lines) {
     for (def line in lines) {
         if (line == "noop") {
             tick++
+            draw(x, tick)
             signalStrengths << x * tick
         } else {
             tick++
+            draw(x, tick)
             signalStrengths << x * tick
             tick++
+            draw(x, tick)
             signalStrengths << x * tick
             x += line.split(" ")[1] as int
         }
@@ -22,5 +25,13 @@ def solution(def lines) {
     signalStrengths[19] + signalStrengths[59] + signalStrengths[99] + signalStrengths[139] + signalStrengths[179] + signalStrengths[219]
 }
 
+void draw(int x, int tick) {
+    int col = (tick - 1) % 40
+    if (col == 0) println()
+
+    if (x == col || x - 1 == col || x + 1 == col)
+        print("#")
+    else print "."
+}
 
 println solution(lines)
